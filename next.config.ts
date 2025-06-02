660029fad524d52f6eda64164857671048193b4c
Sumant <sumant.bagade@moxey.ai>
2025-06-02 22:01:43 +0530

    change reactStrictMode to false

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false, //remove this line in Production
 };
 
 export default nextConfig;
